c9865657c0b3c812f0ec4cdcb347bc528dfdda23
Daniela Cima <trinchera.dm@gmail.com>
2022-12-14 14:51:10 +0100

    last commit

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from './recipes.model';
 import { RecipesService } from './recipes.service';
 
@@ -7,12 +7,29 @@ import { RecipesService } from './recipes.service';
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, OnDestroy {
   recipes: Recipe[];
 
   constructor(private recipesService: RecipesService) {}
 
   ngOnInit() {
+    console.log('ng On Init');
+    console.log(this.recipes);
+  }
+  ionViewWillEnter() {
     this.recipes = this.recipesService.getAllRecipes();
+    console.log('ionViewWillEnter');
+  }
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave');
+  }
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
   }
 }
